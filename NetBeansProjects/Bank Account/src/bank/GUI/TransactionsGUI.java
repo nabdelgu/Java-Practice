13913b8509d60c02510e60b1453249a39f188444
Noah <Noah@76.165.129.115>
2019-05-02 00:43:19 -0500

    Added exception handling that displays a dialog box on error.

--- NetBeansProjects/Bank Account/src/bank/GUI/TransactionsGUI.java
+++ NetBeansProjects/Bank Account/src/bank/GUI/TransactionsGUI.java
@@ -29,106 +29,100 @@ import javafx.stage.Stage;
  */
 public class TransactionsGUI {
 
-    public static void getBankAccount(BankAccount bank) {
-        // Transaction types 
-        final String transactionTypeArray[]
-                = {"Withdraw", "Deposit"};
+    private static Stage window;
+    private static Scene transactionScene;
+    private static Label bankName, routingNumber, accountNumber, balance;
+    private static HBox accountInfoHBox, addDeleteTransactionHBox;
+    private static VBox bankAccountBox;
+    private static TableColumn<Transaction, String> transactionType, transactionAmount, balanceClm;
+    private static TableView<Transaction> bankTransactions;
+    private static TextField amountField, balanceField;
+    private static ComboBox transactionTypeDropdown;
+    private static Button btnAddTransaction, deleteTransaction;
+    private static Transaction transaction;
+    private static final String transactionTyps[] = {"Withdraw", "Deposit"};
 
-        Stage window = new Stage();
+    public static void getBankAccount(BankAccount bank) {
+        window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle("Transactions");
         window.setMinWidth(250);
 
-        Label bankName = new Label();
+        // fill in bank information
+        bankName = new Label();
         bankName.setText("Bank Name: " + bank.getBankName());
-        Label routingNumber = new Label();
+        routingNumber = new Label();
         routingNumber.setText("Routing Number: " + Integer.toString(bank.getRoutingNumber()));
-        Label accountNumber = new Label();
+        accountNumber = new Label();
         accountNumber.setText("Account Number: " + Integer.toString(bank.getAccountNo()));
-        Label balance = new Label();
+        balance = new Label();
         balance.setText("Balance: " + Double.toString(bank.getBalance()));
-
-        HBox accountInfoHBox = new HBox(30);
+        
+        accountInfoHBox = new HBox(30);
         accountInfoHBox.setAlignment(Pos.TOP_CENTER);
         accountInfoHBox.setPadding(new Insets(15, 12, 15, 12));
         accountInfoHBox.getChildren().addAll(bankName, routingNumber, accountNumber, balance);
-
-        TableView<Transaction> bankTransactions;
+        
         bankTransactions = new TableView();
         bankTransactions.setItems(bank.getTransactions());
+        // define table columns
         //Transaction type
-        TableColumn<Transaction, String> transactionType = new TableColumn<>("Transaction Type");
+        transactionType = new TableColumn<>("Transaction Type");
         transactionType.setMinWidth(400);
         transactionType.setCellValueFactory(new PropertyValueFactory<>("transactionType"));
 
         // Transaction Amount
-        TableColumn<Transaction, Double> transactionAmount = new TableColumn<>("Transaction Amount");
+        transactionAmount = new TableColumn<>("Transaction Amount");
         transactionAmount.setMinWidth(200);
         transactionAmount.setCellValueFactory(new PropertyValueFactory<>("transactionAmount"));
 
         //Account Number Column
-        TableColumn<Transaction, Double> balanceClm = new TableColumn<>("Balance");
+        balanceClm = new TableColumn<>("Balance");
         balanceClm.setMinWidth(200);
         balanceClm.setCellValueFactory(new PropertyValueFactory<>("balance"));
 
         //add columns to table
         bankTransactions.getColumns().addAll(transactionType, transactionAmount, balanceClm);
-
-        VBox bankAccountBox = new VBox();
-        //Add bank accounts   table to the VBox
-
-        TextField amountField, balanceField;
-
-        // transactionTypeField = new TextField();
-        // transactionTypeField.setPromptText("Transaction Type");     
-        // Create a combo box for transaction types
-        ComboBox transactionTypeDropdown
-                = new ComboBox(FXCollections
-                        .observableArrayList(transactionTypeArray));
-
+        
+        bankAccountBox = new VBox();
+        //Add bank accounts table to the VBox
+        transactionTypeDropdown = new ComboBox(FXCollections.observableArrayList(transactionTyps));
+        
         amountField = new TextField();
         amountField.setPromptText("Amount");
         balanceField = new TextField();
         balanceField.setPromptText("Balance");
-        Button btnAddTransaction = new Button("Add Transaction");
-        Button deleteTransaction = new Button("Delete");
-        HBox addDeleteTransactionHBox = new HBox();
+        btnAddTransaction = new Button("Add Transaction");
+        deleteTransaction = new Button("Delete");
+        addDeleteTransactionHBox = new HBox();
         addDeleteTransactionHBox.getChildren().addAll(transactionTypeDropdown, amountField, btnAddTransaction, deleteTransaction);
-
+        // add transaction button
         btnAddTransaction.setOnAction(e -> {
-
-            //get the object selected
-            //Transaction t = bankTransactions.getSelectionModel().getSelectedItem();
-            //add a transaction to the bank account
-            // System.out.println(transactionTypeField.getText());
-            // System.out.println(Double.parseDouble(amountField.getText()));
-           // System.out.println(transactionTypeDropdown.getValue().toString());
-         //   System.out.println(bank.getBalance());
-            Transaction t = new Transaction(transactionTypeDropdown.getValue().toString(), Double.parseDouble(amountField.getText()), (bank.getBalance() - Double.parseDouble(amountField.getText())));
-            bank.addTransaction(t);
+            try {
+                transaction = new Transaction(transactionTypeDropdown.getValue().toString(), Double.parseDouble(amountField.getText()), (bank.getBalance() - Double.parseDouble(amountField.getText())));                
+            } catch (NumberFormatException numberFormatException) {
+                AlertError.displayErrorAlert("String found where a number is required");
+            }
+            
+            bank.addTransaction(transaction);
             //set the items on the table
-            //  bankTransactions.setItems(bank.getTransactions());
-            bank.setBalance(t.getTranscationType(), t.getTransactionAmount());
+            bank.setBalance(transaction.getTransactionType(), transaction.getTransactionAmount());
             balance.setText("Balance: " + bank.getBalance());
-            //bankTransactions.setItems(bank.getTransactions());
             e.consume();
         });
-
+        //delete transaction button
         deleteTransaction.setOnAction(e -> {
             //get the bank accounts that are selected
-            Transaction t = bankTransactions.getSelectionModel().getSelectedItems().get(0);
-            bank.removeTransaction(t);
+            transaction = bankTransactions.getSelectionModel().getSelectedItems().get(0);
+            bank.removeTransaction(transaction);
             //remove bank accounts from the list
-            //transactionsSelected.forEach(bank.getTransactions()::remove);
             balance.setText("Balance: " + bank.getBalance());
-
             e.consume();
         });
-
+        // add all item to a VBOX
         bankAccountBox.getChildren().addAll(accountInfoHBox, bankTransactions, addDeleteTransactionHBox);
-        Scene scene = new Scene(bankAccountBox);
-        window.setScene(scene);
+        transactionScene = new Scene(bankAccountBox);
+        window.setScene(transactionScene);
         window.showAndWait();
-
     }
 }
