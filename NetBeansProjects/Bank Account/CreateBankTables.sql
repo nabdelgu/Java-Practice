fbcbba9be564405b034a9dd69a938cc85fa2a8aa
noaha <noaha@10.46.4.201>
2019-05-12 04:18:33 -0500

    removed unessessary file

--- NetBeansProjects/Bank Account/CreateBankTables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `BankAccounts` (
-	`BankName`	TEXT NOT NULL,
-	`RoutingNumber`	INTEGER NOT NULL,
-	`AccountNumber`	INTEGER NOT NULL,
-	`Balance`	INTEGER NOT NULL,
-	PRIMARY KEY(`BankName`,`AccountNumber`)
-);
